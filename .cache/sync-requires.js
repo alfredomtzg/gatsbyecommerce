9d4f39c8f3a181abd2aae0bec653ec108db7c299
Alfredo Martinez <psialfredomtz@gmail.com>
2020-10-10 03:21:47 -0500

    Component thanks is end

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -9,6 +9,6 @@ exports.components = {
   "component---src-pages-404-js": hot(preferDefault(require("/Users/alfredo/Documents/alfredo/gatsbyecommerce/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/alfredo/Documents/alfredo/gatsbyecommerce/src/pages/index.js"))),
   "component---src-pages-page-2-js": hot(preferDefault(require("/Users/alfredo/Documents/alfredo/gatsbyecommerce/src/pages/page-2.js"))),
-  "component---src-pages-thanks-js": hot(preferDefault(require("/Users/alfredo/Documents/alfredo/gatsbyecommerce/src/pages/Thanks.js")))
+  "component---src-pages-thanks-js": hot(preferDefault(require("/Users/alfredo/Documents/alfredo/gatsbyecommerce/src/pages/thanks.js")))
 }
 
--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { SEO } from '../components'
+import { Button, Purchase } from '../styles/components'
+import { Link } from 'gatsby'
 
-export default function Thanks() {
+export default function thanks() {
   return (
     <div>
-
+      <SEO title='Successful purchase' />
+      <Purchase>
+        <h2> Successful purchase</h2>
+        <p>I hope you enjoy your swag, wear it with pride</p>
+        <p>We wait for you back and remember "No pares de aprender" </p>
+        <span role='img' aria-label='emoji' >💚</span>
+        <Link to='/'>
+          <Button>
+            Back to Catalogue
+          </Button>
+        </Link>
+      </Purchase>
     </div>
   )
 }
