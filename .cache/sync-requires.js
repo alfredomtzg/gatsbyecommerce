5bc6baf3e914591027a86296f7ede7d4c81dead6
Alfredo Martinez <psialfredomtz@gmail.com>
2020-10-10 03:01:13 -0500

    agree thanks page

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -8,6 +8,7 @@ exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/alfredo/Documents/alfredo/gatsbyecommerce/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/alfredo/Documents/alfredo/gatsbyecommerce/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/alfredo/Documents/alfredo/gatsbyecommerce/src/pages/index.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/alfredo/Documents/alfredo/gatsbyecommerce/src/pages/page-2.js")))
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/alfredo/Documents/alfredo/gatsbyecommerce/src/pages/page-2.js"))),
+  "component---src-pages-thanks-js": hot(preferDefault(require("/Users/alfredo/Documents/alfredo/gatsbyecommerce/src/pages/Thanks.js")))
 }
 
