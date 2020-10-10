55db973b857b8d59d2c38430503fc1358f70179d
Alfredo Martinez <psialfredomtz@gmail.com>
2020-10-10 16:55:28 -0500

    Petición Graphql

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { StyledJumbo } from '../styles/components'
 
-export default function Jumbo() {
+export default function Jumbo({ description }) {
   return (
     <StyledJumbo>
       <div>
         <h2>Get the best exclusive and special swag from Platzi.</h2>
         <small>Stylish pieces for top students.</small>
+        <br />
+        <small>{description} </small>
       </div>
 
     </StyledJumbo>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,34 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import { Jumbo, SEO } from "../components"
 
-const IndexPage = () => (
-  <>
-    <SEO title="Home" />
-    <Jumbo />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/thanks">Go to thanks</Link>
-    <br></br>
-    <Link to="/sorry">Go to Sorry</Link>
-  </>
-)
+export const query = graphql`
+query GET_DESCRIPTION {
+  allSite {
+    edges {
+      node {
+        siteMetadata {
+          description
+        }
+      }
+    }
+  }
+}
+`
+const IndexPage = ({ data }) =>
+  (
+    <>
+      <SEO title="Home" />
+      <Jumbo description={data.allSite.edges[0].node.siteMetadata.description} />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Link to="/thanks">Go to thanks</Link>
+      <br></br>
+      <Link to="/sorry">Go to Sorry</Link>
+    </>
+  )
+
 
 export default IndexPage
