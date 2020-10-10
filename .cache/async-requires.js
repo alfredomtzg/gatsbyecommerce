5bd3d2ef7f7eeb0d482a0c96bc531b7c3acc6f6a
Alfredo Martinez <psialfredomtz@gmail.com>
2020-10-10 03:29:50 -0500

    finish sorry page

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -6,6 +6,7 @@ exports.components = {
   "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-page-2-js": () => import("./../../../src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */),
+  "component---src-pages-sorry-js": () => import("./../../../src/pages/sorry.js" /* webpackChunkName: "component---src-pages-sorry-js" */),
   "component---src-pages-thanks-js": () => import("./../../../src/pages/thanks.js" /* webpackChunkName: "component---src-pages-thanks-js" */)
 }
 
