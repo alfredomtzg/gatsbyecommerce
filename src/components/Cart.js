ebdf329c6c8f969a3b66399d8d51574d8ca8aefb
Alfredo Martinez <psialfredomtz@gmail.com>
2020-10-12 01:18:14 -0500

    bring Context

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'gatsby'
 import { Button, StyledCart } from '../styles/components'
 import priceFormat from '../utils/priceFormat'
+import { CartContext } from '../Context'
 
 export default function Cart() {
+  const { cart } = useContext(CartContext)
   return (
     <StyledCart>
       <h2> Carito de Compras</h2>
       <table>
         <tbody>
-
+          <tr>
+            <th> Product</th>
+            <th> Price</th>
+            <th> Cantidad</th>
+            <th> Total</th>
+          </tr>
         </tbody>
       </table>
     </StyledCart>
