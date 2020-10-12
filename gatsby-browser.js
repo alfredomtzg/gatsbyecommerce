9fa64c0d3a832edb1aa04df7dd248e40b5aa40f9
Alfredo Martinez <psialfredomtz@gmail.com>
2020-10-12 00:43:31 -0500

    Context API

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,9 +9,10 @@
 const React = require('react')
 const Layout = require('./src/components/layout').default
 const { GlobalStyles } = require('./src/styles/index')
+import { CartProvider } from './src/Context'
 
 exports.wrapRootElement = ({ element }) =>
-  <>
+  <CartProvider >
     <GlobalStyles />
     <Layout>{element}</Layout>
-  </>
+  </CartProvider>
---
+++ src/Context.js
@@ -0,0 +1,21 @@
+import { element } from 'prop-types'
+import React, { createContext, useState } from 'react'
+
+export const CartContext = createContext()
+
+export const CartProvider = ({ children }) => {
+  const [cart, serCart] = useState([])
+
+  const addToCart = element => {
+    serCart([...cart, element])
+  }
+  return (
+    <CartContext.Provider
+      value={
+        cart,
+        addToCart
+      } >
+      {children}
+    </CartContext.Provider>
+  )
+}
