4bac5ee9e7874a4f104a36a7208aa95dbd627c5f
Alfredo Martinez <psialfredomtz@gmail.com>
2020-10-11 20:16:03 -0500

    fixed Color

--- src/components/productDetail.js
+++ src/components/productDetail.js
@@ -11,22 +11,23 @@ import {
 } from '../styles/components'
 import { SEO, Stars } from '.'
 
-export default function ProductDetail({ unit_amount, sku: id, product: { name, metadata } }) {
+export default function ProductDetail({ unit_amount, sku: id, product: { name, metadata: { color, description, wear, img } } }) {
   const formatePrice = priceFormat(unit_amount)
   const [size, setSize] = useState(2)
   const [qty, setQty] = useState(1)
   return (
     <StyledProductDetail>
       <SEO title={name} />
-      <img src={metadata.img} alt={name} />
+      <img src={img} alt={name} />
       <div>
         <Tag>Popular</Tag>
         <h2> {name}</h2>
         <b>USD {formatePrice}</b>
         <Stars />
-        {metadata.wear && <h3>Color: {metadata.color}</h3>}
-        <small>{metadata.description}</small>
-        {metadata.wear && (
+        {wear && <h3> Color: {color} </h3>}
+        <small>{color}</small>
+        <small>{description}</small>
+        {wear && (
           <SizeSelect selected={size}>
             <SizeButton onClick={() => setSize(1)}>XS</SizeButton>
             <SizeButton onClick={() => setSize(2)}>S</SizeButton>
