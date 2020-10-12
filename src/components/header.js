28889556ec8f1d660a6c6411fa8a259870158712
Alfredo Martinez <psialfredomtz@gmail.com>
2020-10-12 00:47:50 -0500

    Consume value in header cart

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,43 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useContext } from "react"
+import { CartContext } from "../Context"
 import { MenuItem, StyledHeader } from '../styles/components'
 
 
-const Header = ({ siteTitle }) => (
-  <StyledHeader>
-    <Link to='/'>
-      <img src='https://i.postimg.cc/6q3pg48v/Logo.png' alt='Logo Platzi' />
-    </Link>
-    <nav>
-      <ul>
-        <MenuItem margin>
-          <Link to='/'>
-            Productos
+const Header = ({ siteTitle }) => {
+  const { cart } = useContext(CartContext)
+  return (
+    <StyledHeader>
+      <Link to='/'>
+        <img src='https://i.postimg.cc/6q3pg48v/Logo.png' alt='Logo Platzi' />
+      </Link>
+      <nav>
+        <ul>
+          <MenuItem margin>
+            <Link to='/'>
+              Productos
           </Link>
-        </MenuItem>
-        <MenuItem margin>
-          <a href="https://alfredomtzg.github.io/challenge-platzi-master/">Alfredo Platzi Master</a>
-        </MenuItem>
-        <MenuItem>
-          <Link to='/cart'>
-            <span>
-              <img src='https://i.postimg.cc/L6wpMxLt/cart.png' alt='LogoCart' />
-            </span>
-          </Link>
-        </MenuItem>
-      </ul>
-    </nav>
-  </StyledHeader>
-)
+          </MenuItem>
+          <MenuItem margin>
+            <a href="https://alfredomtzg.github.io/challenge-platzi-master/">Alfredo Platzi Master</a>
+          </MenuItem>
+          <MenuItem>
+            <Link to='/cart'>
+              <span>
+                <img
+                  src='https://i.postimg.cc/L6wpMxLt/cart.png'
+                  alt='LogoCart'
+                />
+                {cart.length}
+              </span>
+            </Link>
+          </MenuItem>
+        </ul>
+      </nav>
+    </StyledHeader>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
