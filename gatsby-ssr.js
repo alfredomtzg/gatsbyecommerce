e6eecd2e304534d989ed7d83c8e88eec5bd8960c
Alfredo Martinez <psialfredomtz@gmail.com>
2020-10-13 18:19:06 -0500

    create gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,15 @@
  */
 
 // You can delete this file if you're not using it
+
+const React = require('react')
+const Layout = require('./src/components/layout').default
+const { GlobalStyles } = require('./src/styles')
+const { CartProvider } = require('./src/Context')
+
+exports.wrapRootElement = ({ element }) => (
+  <CartProvider >
+    <GlobalStyles />
+    <Layout>{element}</Layout>
+  </CartProvider>
+)
